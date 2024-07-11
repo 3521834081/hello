6e1f11f59211eed935b6019bc4f52e17ab2ee572
3521834081 <3521834081@qq.com>
2024-07-11 21:26:32 +0800

    v-model

--- his_ui/src/views/register/register/index.vue
+++ his_ui/src/views/register/register/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-form v-model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
       <el-form-item label="病历号" prop="caseNumber">
         <el-input
           v-model="queryParams.caseNumber"
