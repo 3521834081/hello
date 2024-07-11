7691544ee87d52aaa9ecb7f13ddc817e109c6157
3521834081 <3521834081@qq.com>
2024-07-11 15:29:23 +0800

    init front

---
+++ his_ui/src/api/jiaofei/jiaofei.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询交费管理列表
+export function listJiaofei(query) {
+  return request({
+    url: '/jiaofei/jiaofei/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询交费管理详细
+export function getJiaofei(id) {
+  return request({
+    url: '/jiaofei/jiaofei/' + id,
+    method: 'get'
+  })
+}
+
+// 新增交费管理
+export function addJiaofei(data) {
+  return request({
+    url: '/jiaofei/jiaofei',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改交费管理
+export function updateJiaofei(data) {
+  return request({
+    url: '/jiaofei/jiaofei',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除交费管理
+export function delJiaofei(id) {
+  return request({
+    url: '/jiaofei/jiaofei/' + id,
+    method: 'delete'
+  })
+}
